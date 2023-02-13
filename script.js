9e2dbea22f6e9ae8aa61dd36c23f762bd2a14b79
Yuliia <yolochka321@gmail.com>
2023-02-13 17:05:16 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+"use strict";
+const speed = document.querySelector(".speed-value");
+const altitude = document.querySelector(".altitude-value");
+const arrow = document.querySelector(".arrow");
+
+navigator.geolocation.watchPosition((data) => {
+  // console.log(data);
+
+  if (data.altitude) {
+    altitude.textContent = data.coords.altitude;
+  } else {
+    altitude.textContent = "0";
+  }
+
+  if (data.speed) {
+    speed.textContent = data.coords.speed;
+  } else {
+    speed.textContent = "0";
+  }
+
+  arrow.style.transform = `rotate(${data.coords.heading}deg)`;
+});
